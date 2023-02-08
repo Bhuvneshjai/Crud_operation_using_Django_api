80de10df164705ec9edbb3f47f334bc3495251a8
Bhuvneshja <bhuvneshjain2000@gmail.com>
2023-02-08 22:56:10 +0530

    reuirements file uploaded

--- Image_Market_Project/Image_Market_Project/settings.py
+++ Image_Market_Project/Image_Market_Project/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
+# This is used for store images and render in html page
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
